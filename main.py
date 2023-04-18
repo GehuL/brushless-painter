bbe69915c23c1ec99bc9082a11114a9e584b6838
mouradlaissaoui <129761351+mouradlaissaoui@users.noreply.github.com>
2023-04-18 23:34:04 +0200

    version 2
    avec détection des fichiers mediapipe

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('homepage.html')
-
+    if os.path.exists('node_modules/@mediapipe/camera_utils/camera_utils.js') and os.path.exists('node_modules/@mediapipe/drawing_utils/drawing_utils.js') \
+            and os.path.exists('node_modules/@mediapipe/control_utils/control_utils.js') and os.path.exists('node_modules/@mediapipe/hands/hands.js'):
+        return render_template('homepage_1.html')
+    else:
+        return render_template('homepage_2.html')
 if __name__ == '__main__':
     app.run(debug=True)
