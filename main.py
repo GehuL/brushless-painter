bbec17851b1f7eae179492a64b4d33d35bdd6d0c
mouradlaissaoui <129761351+mouradlaissaoui@users.noreply.github.com>
2023-04-20 15:47:14 +0200

    ajout de la localisation des scripts en local

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    if os.path.exists('node_modules/@mediapipe/camera_utils/camera_utils.js') and os.path.exists('node_modules/@mediapipe/drawing_utils/drawing_utils.js') \
-            and os.path.exists('node_modules/@mediapipe/control_utils/control_utils.js') and os.path.exists('node_modules/@mediapipe/hands/hands.js'):
-        return render_template('homepage_1.html')
-    else:
-        return render_template('homepage_2.html')
+    return render_template('homepage.html', os=os)
+
 if __name__ == '__main__':
     app.run(debug=True)
